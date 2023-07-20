f81a345f7b6371099e971014a38ef09ccf92ced2
VitalMangal <rvi90@bk.ru>
2023-07-20 12:09:40 +0500

    same fix

--- src/games/runCalculatorGame.js
+++ src/games/runCalculatorGame.js
@@ -1,6 +1,36 @@
 import gameBase from '../index.js';
 import greeting from '../cli.js';
-import calcData from './calcData.js';
+import getRandomInt from '../utils/getRandomInt.js';
+
+const calcData = () => {
+  const result = [];
+  const numberOfRounds = 3;
+  for (let i = 0; i < numberOfRounds; i += 1) {
+    const el1 = getRandomInt();
+    const el2 = getRandomInt();
+    const signs = ['+', '*', '-'];
+    const sign = signs[getRandomInt(0, 2)];
+    const question = `${el1} ${sign} ${el2}`;
+    let answer = 0;
+    switch (sign) {
+      case '+':
+        answer = String(el1 + el2);
+        break;
+      case '*':
+        answer = String(el1 * el2);
+        break;
+      case '-':
+        answer = String(el1 - el2);
+        break;
+      default:
+        answer = NaN;
+        break;
+    }
+    const subResult = [question, answer];
+    result.push(subResult);
+  }
+  return result;
+};
 
 const runCalculatorGame = () => {
   console.log('brain-calc');
