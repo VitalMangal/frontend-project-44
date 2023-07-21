29b110f098529f739aab5be2c53a638bb08533e9
VitalMangal <rvi90@bk.ru>
2023-07-21 11:39:34 +0500

    autosave

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,10 @@
 import readlineSync from 'readline-sync';
 
-const greeting = (gameName, exercise) => {
-  console.log(gameName);
+const greeting = () => {
+  console.log('brain-games');
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  console.log(exercise);
-  return userName;
 };
 
 export default greeting;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import readlineSync from 'readline-sync';
-import greeting from './cli.js';
 
-const runGameBase = (data, gameName, exercise) => {
-  const userName = greeting(gameName, exercise);
+const runGameBase = (getData, gameName, exercise) => {
+  console.log(gameName);
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(exercise);
   const numberOfRounds = 3;
   for (let i = 0; i < numberOfRounds; i += 1) {
-    const dataRound = data();
-    console.log(`Question: ${dataRound[0]}`);
+    const [question, answer] = getData();
+    console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (dataRound[1] === userAnswer) {
+    if (answer === userAnswer) {
       console.log('Correct!');
     } else {
       return console.log(
-        `'${userAnswer}' is wrong answer ;(. Correct answer was '${dataRound[1]}'.\nLet's try again, ${userName}! `,
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.\nLet's try again, ${userName}! `,
       );
     }
   }
